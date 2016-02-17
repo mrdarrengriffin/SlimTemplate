afce56fec072811ad0df561583ac2fb470074b72
Darren Griffin <darren.griffin@live.co.uk>
2016-02-17 19:25:41 +0000

    Removed unnecessary files and code

--- app/views/home.php
+++ app/views/home.php
@@ -3,6 +3,5 @@
 {% block title %}Home{% endblock %}
 
 {% block content %}
-<strong>KLS (UK) Ltd</strong> Routine Service Listings Application v2.6<br>
-<strong>Allowed IPs: </strong><br>{{ config.maintenance.allowed_ips | join(",") }}
+
 {% endblock %}
--- app/views/templates/partials/sidebar.php
+++
@@ -1,9 +0,0 @@
-
-	<div class="panel panel-default">
-	
-		<ul class="list-group">
-			<li class="list-group-item"><a href="{{ urlFor('el.clients') }}">View All Clients</a></li>
-			<li class="list-group-item"><a href="{{ urlFor('el.client.add') }}">Create Client</a></li>
-			
-		</ul>
-	</div>
