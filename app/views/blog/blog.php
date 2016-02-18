891d6888562b95f1ca9ce67b42cdddc0db043aee
Darren Griffin <darren.griffin@live.co.uk>
2016-02-18 12:44:02 +0000

    Initial HTML Framework
    Started outlining the raw HTML frontend structure for a each blog post.

--- app/views/blog/blog.php
+++ app/views/blog/blog.php
@@ -5,7 +5,18 @@
 {% block content %}
 This is the blog page
 
-{% for b in blogItems %}
-{{ b }}
-{% endfor %}
+<div class="blog-container">
+  {% for b in blogItems %}
+  <div class="blog-item">
+    <div class="blog-heading">
+      <span class="blog-title">{{ b.title }}</span>
+      <span class="blog-subtitle">posted by <strong>{{ b.user.username }}</strong> on <strong>{{ b.timestamp_created | date("F jS, Y") }}</strong> at <strong>{{ b.timestamp_created | date("H:ia") }}</strong></span>
+    </div>
+    <div class="blog-content">{{ b.content | raw }}</div>
+    <div class="blog-item-footer">
+      <span class="blog-discuss-reddit">Discuss this post on Reddit</span>
+    </div>
+  </div>
+  {% endfor %}
+</div>
 {% endblock %}
