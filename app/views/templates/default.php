7a6520992a3f6c0a86b22fdd328fc5b10179bed5
Darren Griffin <darren.griffin@live.co.uk>
2016-02-19 14:52:28 +0000

    Installed CKEditor

--- app/views/templates/default.php
+++ app/views/templates/default.php
@@ -5,12 +5,12 @@
 <!--[if gt IE 8]><!-->
 <html>
 	<!--<![endif]-->
-	
+
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<title>{% block title %}Default Title{% endblock %} | {{ application_name }}</title>
-		
+
 		<meta name="description" content="">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<script src="https://code.jquery.com/jquery-3.0.0-alpha1.js"></script>
@@ -19,10 +19,11 @@
 		<link rel="stylesheet" href="{{ base_url }}/public/assets/css/bootstrap.min.css" rel="stylesheet" media="all" />
 		<link rel="stylesheet" href="{{ base_url }}/public/assets/css/theme.css">
 		<link rel="icon" type="image/png" href="{{ base_url }}/icon.png">
-		    <script src="{{ base_url }}/assets/zero/ZeroClipboard.js"></script>
-		
+		<script src="{{ base_url }}/assets/zero/ZeroClipboard.js"></script>
+		<script src="{{ base_url }}/assets/ckeditor/ckeditor.js"></script>
+
 	</head>
-	
+
 	{% block body %}
 	<body>
 		{% endblock %}
@@ -33,11 +34,11 @@
 			{% include 'templates/partials/navigation.php' %}
 			{% include 'templates/partials/messages.php' %}
 			{% endblock %}
-			
+
 			<h3>{{ block('title') }}</h3>
-			
+
 			{% block content %}{% endblock %}
 			{% include 'templates/partials/footer.php' %}
 		</body>
-		
+
 	</html>
