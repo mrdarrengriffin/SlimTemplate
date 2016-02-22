32688abab49a900c65d625ecf310ec0f92e9953f
Darren Griffin <darren.griffin@live.co.uk>
2016-02-22 00:53:13 +0000

    Added navigation link for Minecraft
    and added route name and changed route path

--- app/routes/minecraft/minecraft.php
+++ app/routes/minecraft/minecraft.php
@@ -1,5 +1,5 @@
 <?php
 use MrDarrenGriffin\Minecraft\Minecraft;
-$app->get('/minecraft/tests',function() use ($app){
+$app->get('/minecraft',function() use ($app){
   $app->render('minecraft/minecraft.php');
-});
+})->name('minecraft');
--- app/views/templates/partials/navigation.php
+++ app/views/templates/partials/navigation.php
@@ -16,11 +16,12 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li><a href="{{ urlFor('home') }}">Home</a></li>
+        <li><a href="{{ urlFor('minecraft') }}">Minecraft</a></li>
       </ul>
 	  {% if authEnabled %}
       <ul class="nav navbar-nav navbar-right">
         {% if auth %}
-        
+
         {% if auth.hasPermission('admin.tab.show') %}
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Admin Tools <span class="caret"></span></a>
